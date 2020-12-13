f797c37c4320d0f08064bcc4619919a088a56d6d
pam-param <tamarova.irina@gmail.com>
2020-12-13 23:37:22 +0300

    feat: add date tasks solution

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,22 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+
+  if (year % 4 !== 0) {
+    return false;
+  }
+
+  if (year % 100 !== 0) {
+    return true;
+  }
+
+  if (year % 400 !== 0) {
+    return false;
+  }
+
+  return true;
 }
 
 
@@ -73,8 +87,19 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const SECONDS_PER_MINUTE = 60;
+  let diff = endDate.getTime() - startDate.getTime();
+  const milliseconds = `00${diff % 1000}`.slice(-3);
+  diff = Math.floor(diff / 1000);
+
+  const hours = Math.floor(diff / (SECONDS_PER_MINUTE ** 2));
+  const minutes = Math.floor((diff - SECONDS_PER_MINUTE ** 2 * hours) / SECONDS_PER_MINUTE);
+  const seconds = diff % SECONDS_PER_MINUTE;
+
+  const timeList = [hours, minutes, seconds].map((item) => (item < 10 ? `0${item}` : item));
+
+  return `${timeList.join(':')}.${milliseconds}`;
 }
 
 
@@ -94,8 +119,17 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const myDate = new Date(date);
+  const hours = myDate.getUTCHours();
+  const minutes = myDate.getUTCMinutes();
+  let degrees = Math.abs(0.5 * (60 * (hours % 12) + minutes) - 6 * minutes);
+
+  if (degrees > 180) {
+    degrees -= 180;
+  }
+
+  return degrees * (Math.PI / 180);
 }
 
 
